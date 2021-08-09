cb8aa06d298fec256be4e995eefaa8e4908b3ace
Orion-51 <theta@wolke7.net>
2021-08-09 17:55:39 +0000

    nutsnew

--- notebooks/NUTS.py
+++ notebooks/NUTS.py
@@ -1,83 +1,7 @@
-import scipy.linalg
 import numpy as onp
 
-
-class ExtremeJumpError(RuntimeError):
-    pass
-
 class HMC:
-    def __init__(self, log_post_and_gradient, covariance_estimate, epsilon, steps_per_iteration, limits, args=None, kwargs=None):
-        if kwargs is None:
-            kwargs = {}
-        if args is None:
-            args = []
-        self.fun = log_post_and_gradient
-        self.fun_args = args
-        self.fun_kwargs = kwargs
 
-        # The mid-point.
-        # We use this to transform the boundary planes
-        self.limits = onp.array(limits)
-        self.x0 = onp.array([0.5*(lim[0] + lim[1]) for lim in limits])
-        self.ndim = len(self.x0)
-
-        # We use this to transform between the original space and the
-        # transformed one.  We use this often enough that it's worth
-        # doing the full transform here.
-        # We transform the q coordinates instead of the p momenta,
-        # since otherwise the velocities and the momenta can point
-        # in different directions, and this messes up the reflection
-        # in a way I couldn't figure out.
-        self.L = onp.linalg.cholesky(covariance_estimate)
-        self.Linv = onp.linalg.inv(self.L)
-
-        # Integration parameters
-        self.epsilon = epsilon
-        self.steps_per_iteration = steps_per_iteration
-
-        # We trace the accepted samples
-        self.trace_logP = []
-        self.trace = []
-        self.trace_accept = []
-
-        # As well as tracing the accepted samples it's also nice
-        # to trace the integration paths between those samples.
-        # Improvements to the algorithm make use of those.
-        self.paths = []
-        self.paths_logP = []
-
-        # We need the boundary planes that correspond to the parameter
-        # limits, transformed into our new coordinates.
-        self.lower_bound_planes = []
-        self.upper_bound_planes = []
-        self.bound_normals = []
-
-        for i in range(self.ndim):
-            xb = self.x0.copy()
-
-            # A point on the upper bound plane
-            xb[i] = self.limits[i, 1]
-            uq = self.x2q(xb)
-
-            # A point on the lower bound plane
-            xb[i] = self.limits[i, 0]
-            lq = self.x2q(xb)
-
-            # Normals transform with the inverse of the
-            # point transform.  Forgetting this cost me a lot
-            # of time, until I drew some parallelograms.
-            n = onp.zeros(self.ndim)
-            n[i] = 1.0
-            m = self.L.T @ n
-            # normalize
-            m /= onp.linalg.norm(m)
-
-            self.upper_bound_planes.append(uq)
-            self.lower_bound_planes.append(lq)
-            self.bound_normals.append(m)
-
-
-    
     def get_u(self, q):
         """Compute the posterior and gradient from (and in) normalized coordinates"""
         # convert from normalized q coordinates to x
@@ -89,215 +13,87 @@ class HMC:
         # is linear, so the change to P(x) is just a scaling, so the change to
         # logP is just a constant
         return -logP, -self.L.T @ grad_logP
-        
 
-    def integrate(self, q, p):
+    def leapfrog(self, q, p, epsilon):
+        #get gradient
         U, gradU = self.get_u(q)
-        # Hamiltonian at the start of the integration
-        H0 = 0.5 * (p @ p) + U
-        # I don't really know what symplectic means, but I know
-        # this is it.
-        for i in range(self.steps_per_iteration):
-            # half-update momenta
-            p = p - 0.5 * self.epsilon * gradU
-            
-            # full update q with half-updated p
-            q, p = self.advance_points(q, p)
-            
-            # second half-update momenta
-            U, gradU = self.get_u(q)
-            p = p - 0.5 * self.epsilon * gradU
-            
-            # print out energy levels
-            T = 0.5 * (p @ p)
-            H = T + U
-            dH = H - H0
-            # print(f'U={U:.3f}   T={T:.3f}   H={H:.3f}   ΔH={dH:.3f}')
-            # record a trace of the log_post, -U
-            self.paths_logP.append(-U)
-            # and the chain position
-            x = self.q2x(q)
-            self.paths.append(x)
-        
-        return p, q, U, H, H0
-    
-    
-    def q2x(self, q):
-        """Transform a vector in the space with diagonal mass to a parameter vector"""
-        return self.L @ q + self.x0
-    
-    def x2q(self, x):
-        """Transform a parameter vector to a vector in the space with diagonal mass"""
-        return self.Linv @ (x - self.x0)
-    
-    def extreme_jump_check(self, q, p):
-        """
-        Check for jumps so extreme that it jumps more than twice the bounds.
+        #first half-update
+        p_prime = p + 0.5*epsilon*gradU
+        #update position with half-updated mom
+        q_prime = q + epsilon * p_prime
+        #get new gradient
+        U_new, gradU_new = self.get_u(q_prime)
+        #second half-update
+        p_prime = p_prime + 0.5*epsilon*gradU_new
 
-        Should be rare for most cases, but when it does happen the reflection will stop working.
-        Indicates bad cov estimate or epsilon too large.  Warning below.
+        return q_prime, p_prime
 
-        """
-        q2 = q + self.epsilon * p
-        x = self.q2x(q)
-        low = self.limits[:, 0]
-        high = self.limits[:, 1]
-        upper = 2 * high - low
-        lower = 2 * low - high
-        return onp.any(x > upper) or onp.any(x < lower)
-    
+    def find_reasonable_epsilon(self, q):
+        #set epsilon and an initial random momentum
+        self.epsilon = 1
+        p = onp.random.normal(0,1,len(q))
 
-    def random_kick(self):
-        """Generate a random momentum vector"""
-        mu = onp.zeros(self.ndim)
-        I = onp.eye(self.ndim)
-        p = onp.random.multivariate_normal(mu, I)
-        return p
+        #update p and q
+        q_prime, p_prime = self.leapfrog(q,p)
 
-    
-    def sample(self, n, start=None):
-        """
-        Generate "n" samples from the distribution.
+        #set alpha
+        if
+        #THIS IS UNFINISHED
 
-        Start from "start", if supplied.
-        """
-        if start is None:
-            if self.trace:
-                start = self.trace[-1]
-                print(f"Starting at: {start}")
-            else:
-                raise ValueError("Must supply a starting point the first call to hmc.sample.")
+    def build_tree(self,q,p,u,v,j,epsilon,q_0,p_0):
+        Delta_max = 1000
+        if j == 0:
+            q_prime, p_prime = leapfrog(q,p,v*epsilon)
+
+            U_prime, gradU_prime = self.get_u(q_prime)
+            U_0, gradU_0 = self.get_u(q_0)
+            #get nprime
+            n_prime = int(u < onp.exp(U_prime - 0.5 * onp.dot(p_prime,p_prime.T)))
+            #get sprime
+            s_prime = int(u < (Delta_max + onp.exp(U_prime - 0.5 * onp.dot(p_prime,p_prime.T))))
+
+            #gotta be tricksy about returning here
+            q_minus, q_plus = q_prime, q_prime
+            p_minus, p_plus = p_prime, p_prime
+            alpha_prime = onp.min(1, exp(U - 0.5 * onp.dot(p_prime,p_prime.T) - U_0 + 0.5 * onp.dot(p_0,p_0.T)))
+            nalpha_prime = 1
+
         else:
-            start = onp.array(start)
-        
-        q = self.x2q(start)
-        self.n_accept = 0
-        self.n_reject = 0
-        for j in range(n):
+            #recursion
+            q_minus,p_minus, q_plus, p_plus, q_prime, n_prime, s_prime, alpha_prime, nalpha_prime = build_tree(q,p,u,v,j-1,epsilon,q_0,p_0)
 
-            # Generate a new random momentum vector
-            p = self.random_kick()
+            #step forwards or backwards
+            if s_prime == 1:
+                if v == -1:
+                    q_minus, p_minus, _, _, q_2prime, n_2prime, s_2prime, alpha_2prime, nalpha_2prime = build_tree(q_minus,p_minus,u,v,j-1,epsilon,q_0,p_0)
+                else:
+                    _, _, q_plus, p_plus, q_2prime, n_2prime, s_2prime, alpha_2prime, nalpha_2prime = build_tree(q_plus,p_plus,u,v,j-1,epsilon,q_0,p_0)
 
-            try:
-                # Integrate the trajectory along that path
-                p, q_new, U, H, H0 = self.integrate(q, p)
-            except ExtremeJumpError as err:
-                # We catch a specific error - 
-                print(f"Extreme jump rejected {err}")
-                U = onp.inf
-                H = onp.inf
-                H0 = 0.0
-            # For small enough epsilon these are identical.
-            # The acceptance criterion arises from imperfect integration.
-            deltaH = H - H0
-            log_alpha = -deltaH
-            alpha = onp.exp(log_alpha)
-            p1 = onp.random.uniform()
-            # Standard MH acceptance.
-            accept = log_alpha > onp.log(p1)
-            # Count and update point, and report
-            if accept:
-                self.n_accept += 1
-                q = q_new
-                print(f"Accept {j} alpha={alpha:.2f}  p={p1:.2f}  ΔH={deltaH:.3f}")
-            else:
-                self.n_reject += 1
-                print(f"Reject {j} alpha={alpha:.2f}  p={p1:.2f}  Δh={deltaH:.3f}")
-            # keep a trace of things
-            x = self.q2x(q)
-            self.trace.append(x)
-            self.trace_logP.append(-U)
-            self.trace_accept.append(accept)
+            #accept or reject new step
+            if onp.random.uniform() < (float(n_2prime)/(float(n_prime)+float(n_2prime))):
+                q_prime = q_2prime
 
+            #update acceptance criteria
+            alpha_prime = alpha_prime + alpha_2prime
+            nalpha_prime = nalpha_prime + nalpha_2prime
 
-    def first_boundary_crossing(self, q, p, done):
-        """
-        Locate the first point where the vector connecting
-        q to q+p crosses a boundary of our parameter space.
+            s_prime = s_2prime * int((q_plus - q_minus)*p_minus >= 0) * int((q_plus - q_minus) * p_plus >= 0)
+            n_prime = n_prime + n_2prime
 
-        Report the time to that crossing, the crossing point,
-        and which parameter it happened in.
+        return q_minus, p_minus, q_plus, p_plus, q_prime, n_prime, s_prime, alpha_prime, nalpha_prime
 
-        I better if I was cleverer this would be two lines long
-        or something.
-        """
-        # quick check if we have not crossed any limits
-        q_fin = q + self.epsilon * p
-        x_fin = self.q2x(q_fin)
-        # tmp skip
-        ok = (x_fin > self.limits[:,0]) & (x_fin < self.limits[:,1])
-        if onp.all(ok):
-            return None
-        
-        # If there is a crossing we have to move to the transformed space
-        # to work out where
-        lam = onp.zeros(self.ndim)
-        for i in range(self.ndim):
-            uq = self.upper_bound_planes[i]
-            lq = self.lower_bound_planes[i]
-            m = self.bound_normals[i]
+        def NUTS(self, log_post_and_gradient, q_0, delta, M, Madapt):
+            #set some parameters
+            epsilon_0 = self.find_reasonable_epsilon(q_0)
+            mu = onp.log(10*epsilon_0)
+            eps_bar = 1
+            H_bar = 0
+            gamma = 0.05
+            t_0 = 10
+            kappa = 0.75
+            U, gradU = self.get_u(q_0)
 
-            # distance along p vector to upper bound plane
-            lam[i] = (-(q - uq) @ m) / (p @ m)
-
-            # if this is negative, instead find distance to lower bound plane
-            if not lam[i] > 0:
-                lam[i] = (-(q - lq) @ m) / (p @ m)
-
-        i = lam.argmin()
-        print("added extra 9")
-        q_new = q + lam[i]* 0.999 * p
-        # useful to let the user know where we crossed
-        b = self.q2x(q + lam[i] * p)[i]
-        print(f"Note: parameter {i} reflecting at boundary {b}")
-
-        if i in done:
-            raise ExtremeJumpError("Bounced off the same parameter twice.  Info: "
-                                   f"i = {i}, lam[i] = {lam[i]}, b = {b} p = {p}, q = {q}")
-
-        return q_new, lam[i], i
-
-    def reflect(self, p, i):
-        """
-        Reflect momentum vector p in the boundary plane for parameter i.
-        This works whether it's the upper or lower plane as long as they're
-        perpendicular.
-        """
-        m = self.bound_normals[i]
-        perpendicular = (m @ p) * m
-        parallel = p - perpendicular
-        # flip the perpendicular component
-        perpendicular *= -1
-        return parallel + perpendicular
-
-    def advance_points(self, q, p):
-        """
-        Update the position vector q using momentum p,
-        accounting for any reflections off the boundary of the space.
-        """
-        if self.extreme_jump_check(q, p):
-            raise ExtremeJumpError("Extreme jumps (more than double the range) - check M, epsilon")
-        
-        p = p.copy()
-        q = q.copy()
-        
-        t0 = 0
-        done = {}
-        # locate the first crossing of the bounds, if there is one.
-        # if not this just returns None
-        crossing = self.first_boundary_crossing(q, p, done)
-        while (crossing is not None):
-            q, t_c, i = crossing
-            # Record how much time we have left
-            t0 += t_c
-            # Reflect momentum in plane i
-            p = self.reflect(p, i)
-            done[i] = crossing
-            # We might cross multiple boundaries
-            crossing = self.first_boundary_crossing(q, p, done)
-
-        # any remaining jump, with the newly flipped momentum
-        q = q + (self.epsilon - t0) * p
-
-        return q, p
-        
+            for m in range(1, M):
+                #resample - random kick?
+                p_0 = onp.random.normal(0,1,len(q_0))
+                u = onp.random.uniform(0,onp.exp(U - 0.5 * onp.dot(p_0, p_0.T)))
